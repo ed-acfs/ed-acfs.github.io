2eefa8face5b3f6b8aa8489514d321c5cb25b26e
Cristian Castellari <c.castellari@gmail.com>
2019-09-03 12:05:43 +0200

    Script per il bot Discord

---
+++ discord_bot/yagpdb.xyz/cc_welcome.go
@@ -0,0 +1,37 @@
+/* Custom Command */
+/* Simile al join message, ricorda le regole base del server taggando tutti gli utenti col ruolo "@Membro Flotta"*/
+/* Utilizzo: -welcome */
+
+{{ $icon := (joinStr "" "https://cdn.discordapp.com/icons/" (toString .Guild.ID) "/" .Guild.Icon ".png") }}
+{{ $inara := "https://inara.cz/squadron/4750/"}}
+{{ $website := "https://flottastellare.it" }}
+{{ $fbpage := "https://www.facebook.com/Altocomandoflottastellare"}}
+{{ $advice := (execAdmin "advice") }}
+{{ $topic := (execAdmin "topic") }}
+{{ $catfact := (execAdmin "catfact") }}
+{{ $homesystem := "Wong Sher" }}
+{{ $avatar := (joinStr "" "https://cdn.discordapp.com/avatars/" (toString .User.ID) "/" .User.Avatar ".png") }}
+
+{{$embed := cembed 
+	"title" "Benvenuto sul nostro server Discord. Eccoti qualche informazione utile per iniziare...<:acfs:572729316861804554>" 
+	"description" "\n\n1) Fai richiesta per lo Squadrone ingame, verrà accettata il prima possibile\n\n2) Fai richiesta su [Inara](https://inara.cz/squadron/4750/)\n\n3) Allinea cortesemente il tuo nick al tuo PlayStation ID e scrivilo su <#572400615745912872>\n\n4) Leggi per favore il regolamento su <#555350348013895701> e presentati in <#581916569140068378>\n\nSaremo lieti di accoglierti fra i membri della **Flotta Stellare**" 
+	"color" 4645612 
+	"fields" 
+		(cslice 
+			(sdict "name" "Link Inara" "value" $inara "inline" false) 
+			(sdict "name" "Link al sito web" "value" $website "inline" false) 
+			(sdict "name" "La nostra pagina Facebook" "value" $fbpage "inline" false) 
+			(sdict "name" "Quanti Siamo" "value" (toString .Guild.MemberCount) "inline" true) 
+			(sdict "name" "Il nostro Home System" "value" $homesystem "inline" true) 
+			(sdict "name" "Guild ID" "value" (toString .Guild.ID) "inline" true)) 
+			"author" 
+				(sdict "name" "Alto Comando Flotta Stellare" "url" "https://flottastellare.it" "icon_url" "https://flottastellare.it/assets/icon/apple-icon-76x76.png") 
+			"thumbnail" (sdict "url" $icon) 
+			"footer" 
+				(sdict "text" "YAGPDB.xyz, il tuo assistente di fiducia" "icon_url" "https://cdn.discordapp.com/avatars/204255221017214977/2fa57b425415134d4f8b279174131ad6.png") 
+			"timestamp"  currentTime}}
+
+{{$ref_append := sendMessageNoEscapeRetID nil (joinStr "" "o7 <@&571710879880708097>" ", un breve reminder...")}} /* Qui inserire l'id del ruolo richiesto */
+
+{{ sendMessage nil $embed }}
+{{ deleteTrigger 0 }} /* Cancella il trigger che ha scatenato il messaggio */
---
+++ discord_bot/yagpdb.xyz/leave_message.go
@@ -0,0 +1,2 @@
+Salutiamo **{{.User.Username}}**, che purtroppo ha deciso di lasciarci... :'(
+Fly safe, commander!
