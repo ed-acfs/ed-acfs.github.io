6dd95ed9c52671100c6ae2d1da2a0e8020e86001
Cristian Castellari <c.castellari@gmail.com>
2019-09-18 11:08:19 +0200

    Custo Command '-cok'

---
+++ discord_bot/yagpdb.xyz/cok.go
@@ -0,0 +1,3 @@
+{{deleteTrigger 0}}
+{{ $embed := cembed "title" "Messaggio importante dall'Imperatore:" "image" (sdict "url" "https://flottastellare.it/images/bot/cok.png") }}
+{{ sendMessage nil $embed }}
