f86f8ac5702584ad7e89548a805ad3eb3090abd0
Cristian Castellari <c.castellari@gmail.com>
2020-03-30 14:13:44 +0200

    cc_grecale

---
+++ discord_bot/yagpdb.xyz/cc_grecale.go
@@ -0,0 +1,3 @@
+{{deleteTrigger 0}}
+{{ $embed := cembed "title" "A proposito di armi, pensavo di suggerire..." "image" (sdict "url" "https://flottastellare.it/images/bot/grecale.png") }}
+{{ sendMessage nil $embed }}
--- discord_bot/yagpdb.xyz/cc_membroflotta.go
+++ discord_bot/yagpdb.xyz/cc_membroflotta.go
@@ -1,7 +1,6 @@
 {{/* Custom Command */}}
 {{/* Promuove un utente al ruolo di Membro Flotta e lo notifica sul canale, pingando utente e tutti i Membri */}}
 {{/* Utilizzo: -membroflotta @utente */}}
-
 {{$args := parseArgs 1 ""
   (carg "user" "target")}}
 {{giveRoleID ($args.Get 0).ID 571710879880708097}}
@@ -10,3 +9,7 @@ Congratulazioni {{($args.Get 0).Mention}}! Hai completato il tuo percorso inizia
 Fly safe, CMDR
 
 {{ deleteTrigger 0 }}
+
+{{$embed := (joinStr "" "Congratulazioni  "  .User.Username "! Hai completato il tuo percorso iniziale, diventando Allied con la nostra PMF **Flotta Stellare** e hai quindi conseguito il ruolo di **Membro Ufficiale della Flotta**! Ora vai per favore nel canale <#581171773274324992> e scegli la specializzazione che preferisci, fra Eco, God o Tsd.")}}
+
+{{ sendDM $embed }}
