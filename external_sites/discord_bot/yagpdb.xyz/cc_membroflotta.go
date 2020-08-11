315f68ebaa11ae8dcb3e486d783e9675243ae527
Cristian Castellari <c.castellari@gmail.com>
2020-08-11 12:24:34 +0200

    Upload per discord e bot

--- external_sites/discord_bot/yagpdb.xyz/cc_membroflotta.go
+++ external_sites/discord_bot/yagpdb.xyz/cc_membroflotta.go
@@ -4,6 +4,8 @@
 {{$args := parseArgs 1 ""
   (carg "user" "target")}}
 {{giveRoleID ($args.Get 0).ID 571710879880708097}}
+{{giveRoleID ($args.Get 0).ID 741253469737844858}}{{/* Medagliere */}}
+{{giveRoleID ($args.Get 0).ID 718116478338990130}}{{/* Altri Ruoli */}}
 {{takeRoleID ($args.Get 0).ID 600385868280365059}}
 Congratulazioni {{($args.Get 0).Mention}}! Hai completato il tuo percorso iniziale, diventando **_Allied_** con la nostra PMF **Flotta Stellare** e hai quindi conseguito il ruolo di <@&571710879880708097>! Ora potresti dare uno sguardo al canale <#581171773274324992> e scegliere la specializzazione che preferisci, fra *E.co* (Esploratori), *G.O.D.* (BGS) o *T.S.D.* (Combat e operazioni tattiche).
 La specializzazione non è vincolante e la potrai cambiare in ogni momento.
