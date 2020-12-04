3c17c5573c4b072a32e18753489fdd2408e05237
Cristian Castellari <c.castellari@gmail.com>
2020-12-04 11:54:16 +0100

    Nuovo comando Dany

---
+++ external_sites/discord_bot/yagpdb.xyz/cc_dany.go
@@ -0,0 +1,3 @@
+{{deleteTrigger 0}}
+{{ $embed := cembed "title" "Attenzione! Per la serie: '_Le Grandi Verità della Flotta_', va ora in onda..." "image" (sdict "url" "https://flottastellare.it/images/bot/dany.png") }}
+{{ sendMessage nil $embed }}
