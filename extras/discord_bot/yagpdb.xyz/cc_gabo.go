df6b248786d8050ba3a35fe65be213f252cab808
Cristian Castellari <c.castellari@gmail.com>
2022-05-01 14:30:17 +0200

    Cmando -gabo

---
+++ extras/discord_bot/yagpdb.xyz/cc_gabo.go
@@ -0,0 +1,3 @@
+{{deleteTrigger 0}}
+{{ $embed := cembed "title" "Si si, proprio bravi i cheaters..." "image" (sdict "url" "https://flottastellare.it/images/bot/gabo.png") }}
+{{ sendMessage nil $embed }}
