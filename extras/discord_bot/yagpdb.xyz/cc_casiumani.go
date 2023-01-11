fa068c988750346660a1167103bb9bcb8e2c66a2
Cristian Castellari <c.castellari@gmail.com>
2023-01-11 19:45:41 +0100

    -cc casiumani

---
+++ extras/discord_bot/yagpdb.xyz/cc_casiumani.go
@@ -0,0 +1,4 @@
+{{/* Custom Command */}}
+{{deleteTrigger 0}}
+{{ $embed := cembed "title" "E' bella la socialità, ma..." "image" (sdict "url" "https://flottastellare.it/images/bot/casiumani.png") }}
+{{ sendMessage nil $embed }}
