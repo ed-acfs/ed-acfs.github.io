efcab4e12baec6b4d235b4278fdf7425c1685d9e
Skyflash <c.castellari@gmail.com>
2026-02-28 22:29:41 +0100

    cc_arxamadonna

---
+++ extras/discord_bot/yagpdb.xyz/cc_arxmadonna.go
@@ -0,0 +1,4 @@
+{{/* Custom Command */}}
+{{deleteTrigger 0}}
+{{ $embed := cembed "title" "Pensavi di risparmiare qualche euro? #einvece..." "image" (sdict "url" "https://flottastellare.it/images/bot/arxamadonna.png") }}
+{{ sendMessage nil $embed }}
